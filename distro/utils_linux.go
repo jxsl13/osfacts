536b274962160d4b0f2e74e98f58082a0aaa7673
John Behm <jxsl13@googlemail.com>
2023-01-06 16:32:43 +0100

    cleanup unused functions on windows

--- distro/utils_linux.go
+++ distro/utils_linux.go
@@ -17,3 +17,23 @@ var (
 func stripQuotes(src string) string {
 	return quoteReplacer.Replace(src)
 }
+
+func existsWithSize(filePath string, allowEmpty bool) (bool, error) {
+	fi, err := os.Stat(filePath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return false, nil
+		}
+		return false, err
+	}
+
+	if allowEmpty {
+		return true, nil
+	}
+
+	if fi.Size() == 0 {
+		return false, nil
+	}
+
+	return true, nil
+}
